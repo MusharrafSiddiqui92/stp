592d359c52b59a6ef2cd0e8c562cdf3032dd472a
MusharrafSiddiqui92 <musharrafzafar@gmail.com>
2025-07-22 10:59:24 +0500

    graphical changes

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -18,35 +18,35 @@ const Nav = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon " />
+            <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse bg-blur-primary" style={{ fontSize: '20px' }} id="navbarNavDropdown">
+          <div className="collapse navbar-collapse bg-primary" style={{ fontSize: '20px',opacity:'0.8' }} id="navbarNavDropdown">
             <ul className="navbar-nav w-100 justify-content-center gap-4 pt-1">
               <li className="nav-item">
                 <Link to="/">
-                  <a className="nav-link active" aria-current="page" href="#" style={{fontWeight:'bolder'}}>
+                  <a className="nav-link active text-white" aria-current="page" href="#" style={{fontWeight:'bolder'}}>
                     Home
                   </a>
                 </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link active " href="#" onClick={() => navigate('/gadgets')}  style={{fontWeight:'bolder'}}>
+                <a className="nav-link active text-white" href="#" onClick={() => navigate('/gadgets')}  style={{fontWeight:'bolder'}}>
                   Gadgets
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active " href="#" onClick={() => navigate('/peripheral')}  style={{fontWeight:'bolder'}}>
+                <a className="nav-link active text-white" href="#" onClick={() => navigate('/peripheral')}  style={{fontWeight:'bolder'}}>
                   Peripheral
                 </a>
               </li>
 
               <li className="nav-item">
-                <a className="nav-link active " href="#" onClick={() => navigate('/mobiles')}  style={{fontWeight:'bolder'}}>
+                <a className="nav-link active text-white" href="#" onClick={() => navigate('/mobiles')}  style={{fontWeight:'bolder'}}>
                   Mobiles
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="https://wa.me/+923463825908" target='_blank'  style={{fontWeight:'bolder'}}>
+                <a className="nav-link active text-white" href="https://wa.me/+923463825908" target='_blank'  style={{fontWeight:'bolder'}}>
                   Contact
                 </a>
               </li>
